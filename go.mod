907d4502399fbc89b2fa5710358921417c3787c7
Wallacy <wallacy@snews.tv>
2021-04-08 22:51:52 -0300

    Small improvement: Can select the redirect http port; fix homeDir cert/key file;

--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
-module github.com/carelinus/ssl-proxy
+module github.com/wallacy/ssl-proxy
 
 go 1.15
 
 require (
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
-	golang.org/x/net v0.0.0-20210324051636-2c4c8ecb7826 // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
--- main.go
+++ main.go
@@ -4,32 +4,36 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
-	"github.com/carelinus/ssl-proxy/gen"
-	"github.com/carelinus/ssl-proxy/reverseproxy"
+	"github.com/wallacy/ssl-proxy/gen"
+	"github.com/wallacy/ssl-proxy/reverseproxy"
 	"golang.org/x/crypto/acme/autocert"
 )
 
 var (
-	to           = flag.String("to", "http://127.0.0.1:80", "the address and port for which to proxy requests to")
-	fromURL      = flag.String("from", "127.0.0.1:4430", "the tcp address and port this proxy should listen for requests on")
-	certFile     = flag.String("cert", "", "path to a tls certificate file. If not provided, ssl-proxy will generate one for you in ~/.ssl-proxy/")
-	keyFile      = flag.String("key", "", "path to a private key file. If not provided, ssl-proxy will generate one for you in ~/.ssl-proxy/")
-	domain       = flag.String("domain", "", "domain to mint letsencrypt certificates for. Usage of this parameter implies acceptance of the LetsEncrypt terms of service.")
-	redirectHTTP = flag.Bool("redirectHTTP", false, "if true, redirects http requests from port 80 to https at your fromURL")
-	altnames     = flag.String("altnames", "localhost", "comma separated altnames for the certificate DNS field")
+	to              = flag.String("to", "http://127.0.0.1:80", "the address and port for which to proxy requests to")
+	fromURL         = flag.String("from", "127.0.0.1:4430", "the tcp address and port this proxy should listen for requests on")
+	certFile        = flag.String("cert", "", "path to a tls certificate file. If not provided, ssl-proxy will generate one for you in ~/.ssl-proxy/")
+	keyFile         = flag.String("key", "", "path to a private key file. If not provided, ssl-proxy will generate one for you in ~/.ssl-proxy/")
+	domain          = flag.String("domain", "", "domain to mint letsencrypt certificates for. Usage of this parameter implies acceptance of the LetsEncrypt terms of service.")
+	redirectHTTP    = flag.Int("redirectHTTP", 80, "if set, redirects http requests from provided port to https at your fromURL")
+	altnames        = flag.String("altnames", "localhost", "comma separated altnames for the certificate DNS field")
+	userHomeDir, _  = os.UserHomeDir()
+	defaultCertFile = userHomeDir + "/.ssl-proxy/cert.pem"
+	defaultKeyFile  = userHomeDir + "/.ssl-proxy/key.pem"
 )
 
+// Prefixes
 const (
-	DefaultCertFile = "cert.pem"
-	DefaultKeyFile  = "key.pem"
-	HTTPSPrefix     = "https://"
-	HTTPPrefix      = "http://"
+	HTTPSPrefix = "https://"
+	HTTPPrefix  = "http://"
 )
 
 func main() {
@@ -42,31 +46,41 @@ func main() {
 	// Determine if we need to generate self-signed certs
 	if (!validCertFile || !validKeyFile) && !validDomain {
 		// Use default file paths
-		*certFile = DefaultCertFile
-		*keyFile = DefaultKeyFile
+		*certFile = defaultCertFile
+		*keyFile = defaultKeyFile
 
-		log.Printf("No existing cert or key specified, generating some self-signed certs for use (%s, %s)\n", *certFile, *keyFile)
-
-		// Generate new keys
-		certBuf, keyBuf, fingerprint, err := gen.Keys(365*24*time.Hour, strings.Split(*altnames, ","))
-		if err != nil {
-			log.Fatal("Error generating default keys", err)
+		needCreate := false
+		if _, err := os.Stat(*certFile); os.IsNotExist(err) {
+			needCreate = true
+		} else if _, err := os.Stat(*keyFile); os.IsNotExist(err) {
+			needCreate = true
 		}
 
-		certOut, err := os.Create(*certFile)
-		if err != nil {
-			log.Fatal("Unable to create cert file", err)
+		if needCreate {
+			log.Printf("No existing cert or key specified, generating some self-signed certs for use (%s, %s)\n", *certFile, *keyFile)
+
+			// Generate new keys
+			certBuf, keyBuf, fingerprint, err := gen.Keys(365*24*time.Hour, strings.Split(*altnames, ","))
+			if err != nil {
+				log.Fatal("Error generating default keys", err)
+			}
+
+			certOut, err := create(*certFile)
+			if err != nil {
+				log.Fatal("Unable to create cert file", err)
+			}
+			certOut.Write(certBuf.Bytes())
+
+			keyOut, err := os.OpenFile(*keyFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+			if err != nil {
+				log.Fatal("Unable to create the key file", err)
+			}
+			keyOut.Write(keyBuf.Bytes())
+
+			log.Printf("SHA256 Fingerprint: % X", fingerprint)
+		} else {
+			log.Printf("Found default cert/key files: using...")
 		}
-		certOut.Write(certBuf.Bytes())
-
-		keyOut, err := os.OpenFile(*keyFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-		if err != nil {
-			log.Fatal("Unable to create the key file", err)
-		}
-		keyOut.Write(keyBuf.Bytes())
-
-		log.Printf("SHA256 Fingerprint: % X", fingerprint)
-
 	}
 
 	// Ensure the to URL is in the right form
@@ -89,20 +103,32 @@ func main() {
 	log.Printf(green("Proxying calls from https://%s (SSL/TLS) to %s"), *fromURL, toURL)
 
 	// Redirect http requests on port 80 to TLS port using https
-	if *redirectHTTP {
-		// Redirect to fromURL by default, unless a domain is specified--in that case, redirect using the public facing
+	if *redirectHTTP > 0 {
+		// Redirect to caller host, unless a domain is specified--in that case, redirect using the public facing
 		// domain
 		redirectURL := *fromURL
-		if validDomain {
-			redirectURL = *domain
-		}
+		redirectPort := fmt.Sprintf(":%v", *redirectHTTP)
+
 		redirectTLS := func(w http.ResponseWriter, r *http.Request) {
-			http.Redirect(w, r, "https://"+redirectURL+r.RequestURI, http.StatusMovedPermanently)
+			if validDomain {
+				redirectURL = *domain
+			} else {
+				redirectURL = r.URL.Hostname()
+				if len(redirectURL) <= 0 {
+					host, _, err := net.SplitHostPort(r.Host)
+					if err == nil {
+						redirectURL = host
+					} else {
+						redirectURL = *fromURL
+					}
+				}
+			}
+			http.Redirect(w, r, "https://"+redirectURL+r.RequestURI, http.StatusTemporaryRedirect)
 		}
 		go func() {
 			log.Println(
-				fmt.Sprintf("Also redirecting https requests on port 80 to https requests on %s", redirectURL))
-			err := http.ListenAndServe(":80", http.HandlerFunc(redirectTLS))
+				fmt.Sprintf("Also redirecting https requests on port %s to https requests on %s", redirectPort, redirectURL))
+			err := http.ListenAndServe(redirectPort, http.HandlerFunc(redirectTLS))
 			if err != nil {
 				log.Println("HTTP redirection server failure")
 				log.Println(err)
@@ -142,4 +168,11 @@ func main() {
 // TODO: if more colors used in the future, generalize or pull in an external pkg
 func green(in string) string {
 	return fmt.Sprintf("\033[0;32m%s\033[0;0m", in)
+}
+
+func create(p string) (*os.File, error) {
+	if err := os.MkdirAll(filepath.Dir(p), 0770); err != nil {
+		return nil, err
+	}
+	return os.Create(p)
 }
